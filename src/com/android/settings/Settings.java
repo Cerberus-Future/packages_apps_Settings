0db424a4483538846410aada39e616716c729f03
Naveen <navinkumarachari@gmail.com>
2019-09-25 08:15:24 +0530

    Integrate AtomSettings into Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -175,6 +175,9 @@ public class Settings extends SettingsActivity {
     public static class AppAndNotificationDashboardActivity extends SettingsActivity {}
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
-    public static class SystemDashboardActivity extends SettingsActivity {}
+    
+public static class SystemDashboardActivity extends SettingsActivity {}
+
+public static class AtomSettingsActivity extends SettingsActivity { /* empty */ }
 
 }
